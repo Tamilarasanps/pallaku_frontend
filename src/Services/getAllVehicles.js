46f42704c8771701dfad403e5e31e9a06c90adea
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-08-18 17:35:24 +0530

    jj

--- src/Services/getAllVehicles.js
+++ src/Services/getAllVehicles.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL =
-//   "https://8o289dk2s0.execute-api.ap-south-1.amazonaws.com/default/my-backend-api"; // Adjust port if needed
+  // "https://8o289dk2s0.execute-api.ap-south-1.amazonaws.com/default/my-backend-api"; // Adjust port if needed
 const API_URL = "http://localhost:5000/api/vehicles"; // Adjust port if needed
 
 export const getAllVehicles = async () => {
